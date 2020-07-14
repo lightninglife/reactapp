fab2baa8e5733d198ba1562bd73eaca87074b0fe
Paul Zhao <zhaofeng8711@gmail.com>
2020-07-14 17:45:54 -0400

    initial commit

---
+++ build/precache-manifest.5025f1bd3bc0634b308b6466231b9845.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818ef7183aecac818d742b817564e452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac782ca0362a17bd492",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97b02b3600015bc568d4",
+    "url": "/static/js/2.1a0694e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a0694e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac782ca0362a17bd492",
+    "url": "/static/js/main.8fe7d78e.chunk.js"
+  },
+  {
+    "revision": "71c0237ddcce943e0e5c",
+    "url": "/static/js/runtime-main.ca0af2eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
